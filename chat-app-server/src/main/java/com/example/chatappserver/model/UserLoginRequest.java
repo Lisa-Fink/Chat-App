fcaa3f5c36449851f23ac86c7984d27ffc406b93
lisa fink <lisadfink@gmail.com>
2023-08-14 14:32:12 -0400

    Created UserLoginRequest class for passing in email and password details

---
+++ chat-app-server/src/main/java/com/example/chatappserver/model/UserLoginRequest.java
@@ -0,0 +1,19 @@
+package com.example.chatappserver.model;
+
+public class UserLoginRequest {
+    private String email;
+    private String password;
+
+    public UserLoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
