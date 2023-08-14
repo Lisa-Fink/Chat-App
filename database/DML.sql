f9aad713327cd0c57593d16bbae0e791e70dc9f2
lisa fink <lisadfink@gmail.com>
2023-08-14 13:28:56 -0400

    Added attachments to get all messages query

--- database/DML.sql
+++ database/DML.sql
@@ -75,13 +75,16 @@ UPDATE Channels SET channelName = :newChannelName, roleID = :newRoleID WHERE cha
 -- --------------------------------------------------------------------------------------
 -- Messages
 
--- Get all Messages in a particular channel, get all Reactions to each message including the emojiCode, emojiName, and username
+-- Get all Messages in a particular channel, get all Reactions to each message including the emojiCode, emojiName, and username, get all Attachments to each message
+-- including the attachmentUrl, filename, attachmentID
 SELECT m.messageID, m.text, m.time, m.edited, u.username,
-       r.reactionID, e.emojiCode, e.emojiName
+       r.reactionID, e.emojiCode, e.emojiName,
+       a.attachmentID, a.attachmentUrl, a.filename
 FROM Messages m
 LEFT JOIN Users u ON m.userID = u.userID
 LEFT JOIN Reactions r ON m.messageID = r.messageID
 LEFT JOIN Emojis e ON r.emojiID = e.emojiID
+LEFT JOIN Attachments a ON m.messageID = a.messageID
 WHERE m.channelID = :channelID;
 
 -- Get all Emojis, including its emojiID, emojiCode, and emojiName
