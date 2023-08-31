b6966574d9d66b7dec676e4277b8ac08cf41f07c
lisa fink <lisadfink@gmail.com>
2023-08-31 14:16:12 -0400

    Created functioning buttons/state for Server

--- chat-app-client/src/components/Channels.jsx
+++ chat-app-client/src/components/Channels.jsx
@@ -2,116 +2,134 @@ import React from "react";
 import "../styles/Channels.css";
 import { MdSettings } from "react-icons/md";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useState, useEffect } from "react";
 
-function Channels() {
-  const serverName = "Lisa Chat";
-  const channels = [
-    {
-      channelID: 1,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "General",
-    },
-    {
-      channelID: 2,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Cat Pics",
-    },
-    {
-      channelID: 3,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Leetcode",
-    },
-    {
-      channelID: 4,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Random",
-    },
-    {
-      channelID: 5,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Tech Talk",
-    },
-    {
-      channelID: 6,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Funny Memes",
-    },
-    {
-      channelID: 7,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Music",
-    },
-    {
-      channelID: 8,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Movies",
-    },
-    {
-      channelID: 9,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Books",
-    },
-    {
-      channelID: 10,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Food",
-    },
-    {
-      channelID: 11,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Gaming",
-    },
-    {
-      channelID: 12,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Travel",
-    },
-    {
-      channelID: 13,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Fitness",
-    },
-    {
-      channelID: 14,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Art",
-    },
-    {
-      channelID: 15,
-      serverID: 1,
-      roleID: 4,
-      channelTypeID: 1,
-      channelName: "Science",
-    },
-  ];
+function Channels({ server, serverName }) {
+  // store channels mapped to serverID
+  const channelsHardcoded = {
+    1: [
+      {
+        channelID: 1,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "General",
+      },
+      {
+        channelID: 2,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Cat Pics",
+      },
+      {
+        channelID: 3,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Leetcode",
+      },
+      {
+        channelID: 4,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Random",
+      },
+      {
+        channelID: 5,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Tech Talk",
+      },
+      {
+        channelID: 6,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Funny Memes",
+      },
+      {
+        channelID: 7,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Music",
+      },
+      {
+        channelID: 8,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Movies",
+      },
+      {
+        channelID: 9,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Books",
+      },
+      {
+        channelID: 10,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Food",
+      },
+      {
+        channelID: 11,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Gaming",
+      },
+      {
+        channelID: 12,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Travel",
+      },
+      {
+        channelID: 13,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Fitness",
+      },
+      {
+        channelID: 14,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Art",
+      },
+      {
+        channelID: 15,
+        serverID: 1,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Science",
+      },
+    ],
+    3: [
+      {
+        channelID: 16,
+        serverID: 3,
+        roleID: 4,
+        channelTypeID: 1,
+        channelName: "Science",
+      },
+    ],
+  };
+
+  const [channels, setChannels] = useState(channelsHardcoded[server]);
+  useEffect(() => {
+    setChannels(channelsHardcoded[server]);
+  }, [server]);
+  // if server changes, channels needs to be updated to display the channels of the new server
 
   const channelList = channels.map((channel) => {
     return <li key={channel.channelID}># {channel.channelName}</li>;
--- chat-app-client/src/components/Servers.jsx
+++ chat-app-client/src/components/Servers.jsx
@@ -1,70 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Servers.css";
-function Servers() {
-  const servers = [
-    {
-      serverID: 1,
-      serverName: "Lisa's Chat",
-      serverDescription: "Test 1",
-      serverImageUrl: null,
-    },
-    {
-      serverID: 2,
-      serverName: "The Hangout",
-      serverDescription: "Hangout for friends",
-      serverImageUrl: "./images/cat2.jpg",
-    },
-    {
-      serverID: 3,
-      serverName: "Magic The Gathering Players",
-      serverDescription: "A Server for Magic",
-      serverImageUrl: null,
-    },
-    {
-      serverID: 1,
-      serverName: "Lisa's Chat",
-      serverDescription: "Test 1",
-      serverImageUrl: null,
-    },
-    {
-      serverID: 2,
-      serverName: "The Hangout",
-      serverDescription: "Hangout for friends",
-      serverImageUrl: "./images/cat2.jpg",
-    },
-    {
-      serverID: 3,
-      serverName: "Magic The Gathering Players",
-      serverDescription: "A Server for Magic",
-      serverImageUrl: null,
-    },
-    {
-      serverID: 3,
-      serverName: "Magic The Gathering Players",
-      serverDescription: "A Server for Magic",
-      serverImageUrl: null,
-    },
-  ];
+function Servers({ servers, server, setServer, setServerName }) {
+  const [showServerDetails, setShowServerDetails] = useState(0);
+
+  const handleServerClick = (e) => {
+    const serverID = parseInt(e.currentTarget.dataset.serverId);
+    const serverName = e.currentTarget.dataset.serverName;
+    setServer(serverID);
+    setServerName(serverName);
+  };
+
+  const handleServerHover = (e) => {
+    const serverID = parseInt(e.currentTarget.dataset.serverId);
+    setShowServerDetails(serverID);
+  };
+
+  const handleServerEndHover = () => {
+    setShowServerDetails(0);
+  };
 
   const thumbnails = servers.map((server) => {
     return (
       <li key={server.serverID}>
-        {server.serverImageUrl !== null ? (
-          <img className="server-thumbnail" src={server.serverImageUrl} />
-        ) : (
-          <div className="letter-thumbnail">
-            {server.serverName.substring(0, 1)}
-          </div>
+        <button
+          className="server-thumbnail"
+          data-server-id={server.serverID}
+          data-server-name={server.serverName}
+          onClick={handleServerClick}
+          onMouseEnter={handleServerHover}
+          onMouseLeave={handleServerEndHover}
+        >
+          {server.serverImageUrl !== null ? (
+            <img className="image-thumbnail" src={server.serverImageUrl} />
+          ) : (
+            server.serverName.substring(0, 1)
+          )}
+        </button>
+        {showServerDetails === server.serverID && (
+          <div className="server-details">{server.serverName}</div>
         )}
       </li>
     );
   });
 
   return (
-    <div className="servers">
+    <ul className="servers">
       {thumbnails}
-      <div className="add-thumbnail">+</div>
-    </div>
+      <li>
+        <button className="server-thumbnail add-thumbnail">+</button>
+      </li>
+    </ul>
   );
 }
 
