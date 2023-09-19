0e13310f8050a1724758c478e164f4b78aa336a1
lisa fink <lisadfink@gmail.com>
2023-09-19 13:37:24 -0400

    Changed to combinedReducer to reset all on logout

--- chat-app-client/src/redux/store.js
+++ chat-app-client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import serversReducer from "./serversSlice";
 import channelsReducer from "./channelsSlice";
 import messagesReducer from "./messagesSlice";
@@ -6,13 +6,22 @@ import usersReducer from "./usersSlice";
 import currentReducer from "./currentSlice";
 import authReducer from "./authSlice";
 
+const combinedReducer = combineReducers({
+  servers: serversReducer,
+  channels: channelsReducer,
+  messages: messagesReducer,
+  users: usersReducer,
+  current: currentReducer,
+  auth: authReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === "auth/logout") {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
+
 export default configureStore({
-  reducer: {
-    servers: serversReducer,
-    channels: channelsReducer,
-    messages: messagesReducer,
-    users: usersReducer,
-    current: currentReducer,
-    auth: authReducer,
-  },
+  reducer: rootReducer,
 });
