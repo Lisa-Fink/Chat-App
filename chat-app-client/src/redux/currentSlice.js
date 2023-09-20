0e0d95c0b7209c43ca0741571fb8d2c12bc4dcc6
lisa fink <lisadfink@gmail.com>
2023-09-20 16:38:32 -0400

    Added roleID to current channel

--- chat-app-client/src/redux/currentSlice.js
+++ chat-app-client/src/redux/currentSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   server: {}, // {id, name}
-  channel: {}, // {id, name}
+  channel: {}, // {id, name, roleID}
 };
 
 const curSlice = createSlice({
